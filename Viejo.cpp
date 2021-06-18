6e7001afc5d60996475a68bd2058f65e18613aa1
Pierina Fraire <pierinafrairr@gmail.com>
2021-06-18 13:49:43 -0300

    Implementacion Viejo cpp

--- Viejo.cpp
+++ Viejo.cpp
@@ -1,6 +1,8 @@
 #include "Viejo.h"
 
-Viejo::Viejo(const string codigo_colec, unsigned int cant_max_pas, unsigned int direccion, float peso_max, const float tarifa, Ramal* ramal)
+Viejo::Viejo(string codigo_colec, unsigned int cant_max_pas, unsigned int direccion,
+	float peso_max, Ramal* ramal): Colectivo(codigo_colec, cant_max_pas, direccion,
+		peso_max, ramal)
 {
 }
 
